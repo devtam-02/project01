16b7f21f4f04c55053889490e388af676eb01924
Nttam22x <Nguyentrongtam2x2@gmail.com>
2023-03-09 11:49:28 +0700

    update project

---
+++ src/main/java/com/devTam/project01/Config/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.devTam.project01.Config;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.devTam.project01.Entity.Student;
+import com.devTam.project01.Respository.StudentRepository;
+
+@Configuration
+public class StudentConfig {
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository repository) {
+		return args ->{
+			Student tam = new Student( 
+					"Nguyen Trong Tam",
+					"nguyentrongtam2x2@gmail.com",
+					LocalDate.of(2000, Month.JANUARY, 5),
+					21);
+			Student nam = new Student( 
+					"Hoang Duc Nam",
+					"namhoang22@gmail.com",
+					LocalDate.of(2002, Month.JUNE, 3),
+					21);
+			repository.saveAll(List.of(tam,nam));
+		};
+	}
+}
